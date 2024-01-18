b6920f4f8cf96656d51492fd241fa19283b85966
MarceauLp <marceau.lp@gmail.com>
2024-01-18 16:29:06 +0100

    Fix

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,6 +5,7 @@ import views.MeteoView2;
 public class Main {
 
     public static void main(String[] args) {
+		// Etape 3: Instancier le simulateur et les vues
 	WeatherSimulator simulateur = new WeatherSimulator();
 	MeteoView1 view1 = new MeteoView1("METEO V1");
 	MeteoView2 view2 = new MeteoView2("METEO V2");
